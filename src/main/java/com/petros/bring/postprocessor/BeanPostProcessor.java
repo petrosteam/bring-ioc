b3a499a9811844ed515458d0ae0184ed3c2a8f04
mykola.demchenko <mykola.demchenko@kyivstar.ua>
2023-02-06 16:32:16 +0200

    PTR-27 Check circular dependency. Add java docs for some interfaces

--- src/main/java/com/petros/bring/postprocessor/BeanPostProcessor.java
+++ src/main/java/com/petros/bring/postprocessor/BeanPostProcessor.java
@@ -1,11 +1,29 @@
 package com.petros.bring.postprocessor;
 
+/**
+ * BeanPostProcessor makes it possible to apply custom modification for created beans.
+ * Usually post processors that populate beans via marker interfaces implement postProcessBeforeInitialization,
+ * and post processors that wrap beans with proxies - should implement postProcessAfterInitialization.
+ */
 public interface BeanPostProcessor {
 
+    /**
+     * Method is processed before @PostConstruct. Usually uses for dependency injection, filling properties
+     * @param bean - bean, that should be modified
+     * @param beanClass - class of the bean
+     * @return - original bean, not a proxy
+     */
     default Object postProcessBeforeInitialization(Class<?> beanClass, Object bean) {
         return bean;
     }
 
+    /**
+     * Method is processed after @PostConstruct. Used when custom logic around bean methods is needed.
+     * For example with @Cacheable, @Async, @Transactional...
+     * @param bean - bean, that should be modified
+     * @param beanClass - class of the bean
+     * @return - bean wrapped with proxy
+     */
     default Object postProcessAfterInitialization(Class<?> beanClass, Object bean) {
         return bean;
     }
