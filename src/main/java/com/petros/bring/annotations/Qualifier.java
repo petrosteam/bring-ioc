6fc9a00fbcfcce2c1ee3d4742f0b36a339c01e61
Oleksii Onyshchenko <35780738+Arrrmy21@users.noreply.github.com>
2023-02-11 21:07:57 +0200

    [PTR-16] Describe in JavaDoc(@Qualifier) & Readme.md (#32)
    Co-authored-by: Oleksii Onyshchenko <oleksii.onyshchenko@biscience.com>

--- src/main/java/com/petros/bring/annotations/Qualifier.java
+++ src/main/java/com/petros/bring/annotations/Qualifier.java
@@ -5,6 +5,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Qualifier - annotation that may be used on a field only to clarify which bean to inject from the context.
+ * <p>
+ * Using with {@link Autowired} annotation.
+ * <p>
+ * Usage example:
+ * <pre>{@code
+ * public class JavadocExampleClass {
+ *
+ *   @Autowired
+ *   @Qualifier("distinctClassName")
+ *   public ExampleService exampleService;
+ * }}</pre>
+ */
 @Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Qualifier {
