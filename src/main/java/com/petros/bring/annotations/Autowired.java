c17c4740cfa1520652e0fee5cf08561248ca8a4e
Oleksii Onyshchenko <35780738+Arrrmy21@users.noreply.github.com>
2023-02-11 17:53:45 +0200

    [PTR-16] Describe in JavaDoc '@Autowired' & Readme.md (#31)
    Co-authored-by: Oleksii Onyshchenko <oleksii.onyshchenko@biscience.com>

--- src/main/java/com/petros/bring/annotations/Autowired.java
+++ src/main/java/com/petros/bring/annotations/Autowired.java
@@ -1,16 +1,40 @@
 package com.petros.bring.annotations;
 
-import org.apache.commons.lang3.StringUtils;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * The annotation Autowired.
- * Used for annotation fields and methods to initialize by beans from context
+ * Annotation allows to mark a field property or a method with only one class as parameter.
+ * Marked variable or a parameter must be a class.
+ * <p>
+ * Marked class will be taken from an application context loaded by {@link com.petros.bring.bean.factory.BeanFactory}.
+ * <p>Injected field of a method must be used in classes annotated with: {@link Component} or {@link Configuration}.
+ * <p>
+ * To inject class with a certain name - use {@link Qualifier}.
+ * <p>
+ * Example of field autowiring:
+ * <pre>{@code
+ * public class JavadocExampleClass {
+ *
+ *   @Autowired
+ *   public ExampleService exampleService;
+ * }}</pre>
+ * <p>
+ * Example of method autowiring:
+ * <pre>{@code
+ * public class JavadocExampleClass {
+ *
+ *   private ExampleService exampleService;
+ *
+ *   @Autowired
+ *   public void setExampleService(ExampleService exampleService) {
+ *      this.exampleService = exampleService;
+ *   }
+ * }}</pre>
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD , ElementType.METHOD})
-public @interface Autowired {}
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface Autowired {
+}
